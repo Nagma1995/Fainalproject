e58aab446e539808ae3bb6dfc38cad1b7d1a1261
Nagma1995 <mothanagma1995@gmail.com>
2025-04-01 23:45:56 +0530

    done
    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./Component";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
+
+export default App;
